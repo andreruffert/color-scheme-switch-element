f552297fd99a8bb04fc11a9f216c876f0e4050ef
André Ruffert <464300+andreruffert@users.noreply.github.com>
2026-08-09 14:04:47 +0200

    test: introduce tests

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { playwright } from '@vitest/browser-playwright';
 import { defineConfig } from 'vite';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -11,6 +12,18 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/index.js'),
       name: 'ColorSchemeSwitch',
       fileName: 'color-scheme-switch-element',
+    },
+  },
+  test: {
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: playwright(),
+      instances: [
+        {
+          browser: 'chromium',
+        },
+      ],
     },
   },
 });
